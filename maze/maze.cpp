f5979b6de99ee8982e018e57c6592834ddf81869
Gleb Grenkin <glebgrenkin@gmail.com>
2019-11-27 14:50:16 +1000

    Случайная генерация лабиринта

--- maze/maze.cpp
+++ maze/maze.cpp
@@ -8,8 +8,10 @@
 using namespace std;
 
 /***********************************************/
+// закомментировать в Linux
 #define WIN_CONSOLE
 
+// символы для вывода на экран
 enum console_chars {
 #ifdef WIN_CONSOLE
     CH_POINT      = 249,
@@ -100,6 +102,37 @@ void Maze::print ()
             //write(CH_SPACE);
         }
         write(CH_LINE_BREAK);
+    }
+}
+
+
+// бинарная случайная величина
+//   1 - с вероятностью p%, 0 - с вероятностью (1-p)%
+bool random_binary (int p)
+{
+    int random_value = rand();  // 0..RAND_MAX
+    return (random_value < (double)RAND_MAX * p / 100);
+}
+
+// случайная генерация лабиринта
+//   каждая стенка ставится с вероятностью prob процентов
+//   srand() не вызывается
+void Maze::generate_random (int prob)
+{
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            // перечисляем все внутренние стенки:
+            //   справа и снизу от клеточек (i, j), i=1..n-1, j=1..m-1
+            //   справа от клеточек (i, j), i=n, j=1..m-1
+            //   снизу от клеточек (i, j), i=1..n-1, j=m
+
+            // с вероятностью prob% ставим стенку справа от клеточки (i, j)
+            if (j < m && random_binary(prob))
+                walls_v[i][j] = true;
+            // с вероятностью prob% ставим стенку под клеточкой (i, j)
+            if (i < n && random_binary(prob))
+                walls_h[i][j] = true;
+        }
     }
 }
 
--- run_maze.cpp
+++ run_maze.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "maze/maze.h"
 
 using namespace std;
@@ -6,11 +8,18 @@ using namespace std;
 int main()
 {
     Maze maze(5, 5);
+
     // добавляем стенки
-    maze.walls_v[1][1] = maze.walls_v[2][1] = true;
+    /* maze.walls_v[1][1] = maze.walls_v[2][1] = true;
     maze.walls_h[2][2] = maze.walls_h[2][3] =
         maze.walls_h[2][4] = maze.walls_h[2][5] = true;
-    maze.walls_v[4][3] = maze.walls_v[5][3] = true;
+    maze.walls_v[4][3] = maze.walls_v[5][3] = true; */
+
+    int p = 25;
+    srand (time(NULL));
+    // случайная генерация лабиринта
+    //   ставим стенку с вероятностью p%
+    maze.generate_random(p);
 
     maze.print();
 
